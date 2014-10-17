12227d7df4d0033b05e25393db02fe2e0a8e4422
stacybanks514 <stacybanks514@gmail.com>
2014-10-17 10:15:31 -0400

    Some additions, changed the google search link

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
     scope :most_recent, -> { limit(10).order('published_at desc') }
     scope :unpublished, -> { where("published_at > '#{Date.today}'")}
-
+    
     def self.search(q)
-      result = Post.where(['body LIKE ? OR subject LIKE ?', "%#{q}%", "%#{q}%"])
+      where(['body LIKE ? OR subject LIKE ?', "%#{q}%", "%#{q}%"])
     end 
 end
 
